2cd8a38f73d502f1a98dd0a85b20f32511237e86
Md Farhan Sayeed <fs10956@gmail.com>
2024-10-31 13:52:34 +0530

    Added client config

---
+++ Blog App/client/src/componets/Header.js
@@ -0,0 +1,122 @@
+import React, { useReducer } from "react";
+import { Link } from "react-router-dom";
+import { authActions, setDarkmode } from "../store";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { useState } from "react";
+import { lightTheme, darkTheme } from "../utils/theme";
+
+const Header = () => {
+  const dispath = useDispatch();
+  const isDark = useSelector((state) => state.theme.isDarkmode);
+  const theme = isDark ? darkTheme : lightTheme;
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const [value, setValue] = useState();
+
+  return (
+    <AppBar position="sticky" sx={{ background: `${theme.bg}` }}>
+      <Toolbar>
+        <Typography variant="h4">BlogsApp</Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab
+                //className={classes.font}
+                LinkComponent={Link}
+                to="/blogs"
+                label="All Blogs"
+              />
+              <Tab
+                //className={classes.font}
+                LinkComponent={Link}
+                to="/myBlogs"
+                label="My Blogs"
+              />
+              <Tab
+                //className={classes.font}
+                LinkComponent={Link}
+                to="/blogs/add"
+                label="Add Blog"
+              />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Button
+                LinkComponent={Link}
+                to="login/"
+                sx={{
+                  margin: 1,
+                  fontWeight: "bold",
+                  color: "white",
+                  borderRadius: 10,
+                }}
+              >
+                Login
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="login/"
+                sx={{
+                  margin: 1,
+                  fontWeight: "bold",
+                  color: "white",
+                  borderRadius: 10,
+                }}
+              >
+                SignUp
+              </Button>
+            </>
+          )}
+
+          {isLoggedIn && (
+            <Button
+              onClick={() => dispath(authActions.logout())}
+              LinkComponent={Link}
+              to="/login"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+              color="warning"
+            >
+              Logout
+            </Button>
+          )}
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+              dispath(setDarkmode(!isDark));
+            }}
+            style={{
+              alignContent: "center",
+              padding: "10px 0",
+              cursor: "pointer",
+            }}
+          >
+            {isDark ? <LightModeIcon /> : <DarkModeIcon />}
+          </div>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ Blog App/client/src/store/index.js
@@ -0,0 +1,40 @@
+import { combineReducers, configureStore, createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { isLoggedIn: false },
+  reducers: {
+    login(state) {
+      console.log("updating");
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      localStorage.removeItem("userId");
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    isDarkmode: false,
+  },
+  reducers: {
+    setDarkmode: (state, action) => {
+      state.isDarkmode = action.payload;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+export const setDarkmode = themeSlice.actions.setDarkmode;
+
+const rootReducer = combineReducers({
+  auth: authSlice.reducer,
+  theme: themeSlice.reducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
